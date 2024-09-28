0fbc9b9aa53d5c6e0e2c93b79e7f2c41da66775e
koan <koan329@gmail.com>
2024-09-28 07:08:47 -0400

    Merge branch 'refs/heads/elvis-branch'
    # Conflicts:
    #	app/src/main/java/com/example/hackgt/View/MainActivity.java

--- app/src/main/java/com/example/hackgt/View/AnimalActivity.java
+++ app/src/main/java/com/example/hackgt/View/AnimalActivity.java
@@ -1,6 +1,9 @@
 package com.example.hackgt.View;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +25,14 @@ public class AnimalActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button animalBackButton = findViewById(R.id.back_button_animal);
+        animalBackButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnimalActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+            }
+        }));
     }
 }
