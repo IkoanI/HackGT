9595f5f682aa20c087e917d47de3bd24b3637ea8
koan <koan329@gmail.com>
2024-09-28 08:26:18 -0400

    Merge branch 'refs/heads/elvis-branch'
    # Conflicts:
    #	app/src/main/java/com/example/hackgt/View/MainActivity.java

---
+++ app/src/main/java/com/example/hackgt/View/Player.java
@@ -0,0 +1,8 @@
+package com.example.hackgt.View;
+
+public class Player {
+    public static int coins = 0;
+    public static int baseSteps = 0;
+    public static int spent = 0;
+    public static int[] items = {0, 0 ,0};
+}
