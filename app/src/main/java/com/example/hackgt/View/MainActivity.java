401758f97dbe5f5a88fd5b435cdfbe5bd4eaa5ba
koan <koan329@gmail.com>
2024-09-27 21:26:34 -0400

    Added basic step tracker

--- app/src/main/java/com/example/hackgt/View/MainActivity.java
+++ app/src/main/java/com/example/hackgt/View/MainActivity.java
@@ -20,13 +20,22 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.fitness.FitnessLocal;
 import com.google.android.gms.fitness.LocalRecordingClient;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.LocalBucket;
+import com.google.android.gms.fitness.data.LocalDataPoint;
+import com.google.android.gms.fitness.data.LocalDataSet;
 import com.google.android.gms.fitness.data.LocalDataType;
+import com.google.android.gms.fitness.data.LocalField;
+import com.google.android.gms.fitness.data.LocalValue;
 import com.google.android.gms.fitness.request.LocalDataReadRequest;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity {
     public static final int ACTIVITY_REQUEST = 1;
@@ -59,38 +68,48 @@ public class MainActivity extends AppCompatActivity {
                 // for ActivityCompat#requestPermissions for more details.
                 String[] permissions = {Manifest.permission.ACTIVITY_RECOGNITION};
                 ActivityCompat.requestPermissions(this, permissions, ACTIVITY_REQUEST);
-                return;
             }
-            else{
-                Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show();
-                fitnessClient.subscribe(LocalDataType.TYPE_STEP_COUNT_DELTA)
-                        .addOnSuccessListener(b -> {
-                            Toast.makeText(this, "Subscribed!", Toast.LENGTH_SHORT).show();
-                        })
-                        .addOnFailureListener(b -> {
-                            Toast.makeText(this, "Failed to subscribe!", Toast.LENGTH_SHORT).show();
-                        });
+            Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show();
+            fitnessClient.subscribe(LocalDataType.TYPE_STEP_COUNT_DELTA)
+                    .addOnSuccessListener(b -> {
+                        Toast.makeText(this, "Subscribed!", Toast.LENGTH_SHORT).show();
+                    })
+                    .addOnFailureListener(b -> {
+                        Toast.makeText(this, "Failed to subscribe!", Toast.LENGTH_SHORT).show();
+                    });
 
-                ZonedDateTime endTime = LocalDateTime.now().atZone(ZoneId.systemDefault());
-                ZonedDateTime startTime = endTime.minusWeeks(1);
-                LocalDataReadRequest readRequest = new LocalDataReadRequest.Builder()
-                        .aggregate(LocalDataType.TYPE_STEP_COUNT_DELTA)
-                        .bucketByTime(1, TimeUnit.DAYS)
-                        .setTimeRange(startTime.toEpochSecond(), endTime.toEpochSecond(), TimeUnit.SECONDS)
-                        .build();
+            ZonedDateTime endTime = LocalDateTime.now().atZone(ZoneId.systemDefault());
+            ZonedDateTime startTime = endTime.minusWeeks(1);
+            LocalDataReadRequest readRequest = new LocalDataReadRequest.Builder()
+                    .aggregate(LocalDataType.TYPE_STEP_COUNT_DELTA)
+                    .bucketByTime(1, TimeUnit.DAYS)
+                    .setTimeRange(startTime.toEpochSecond(), endTime.toEpochSecond(), TimeUnit.SECONDS)
+                    .build();
 
-                fitnessClient.readData(readRequest)
-                        .addOnSuccessListener(r -> {
-                            for(int x : r.getBuckets().stream().flatMap()){
-
-                            }
-                        })
-                        .addOnFailureListener(r -> {
-                            Toast.makeText(this, "Failed to read data!", Toast.LENGTH_SHORT).show();
-                        });
-
+            fitnessClient.readData(readRequest)
+                    .addOnSuccessListener(r -> {
+                        Toast.makeText(this, "SUCCESS", Toast.LENGTH_SHORT).show();
+                        dumpDataSet(r.getBuckets().get(0).getDataSets().get(0));
+                    })
+                    .addOnFailureListener(r -> {
+                        Toast.makeText(this, "Failed to read data!", Toast.LENGTH_SHORT).show();
+                    });
             }
         }
 
-    }
+        public void dumpDataSet(LocalDataSet dataSet){
+            String TAG = "TESTING";
+            Log.i(TAG, "Data returned for Data type: " + dataSet.getDataType().getName());
+            for (LocalDataPoint dp : dataSet.getDataPoints()) {
+                Log.i(TAG,"Data point:");
+                Log.i(TAG,"\tType: " + dp.getDataType().getName());
+                Log.i(TAG,"\tStart: " + dp.getStartTime(TimeUnit.HOURS));
+                Log.i(TAG,"\tEnd: " + dp.getEndTime(TimeUnit.HOURS));
+                for (LocalField field : dp.getDataType().getFields()) {
+                    Log.i(TAG,"\tLocalField: " + field.getName() +
+                            " LocalValue: " + dp.getValue(field));
+                }
+            }
+        }
 }
+
