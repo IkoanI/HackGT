df55e089aa8694dd345b967f6426735b63b1b6c8
elvisli <elvisli030405@gmail.com>
2024-09-28 03:08:15 -0400

    real time step tracking

--- app/src/main/java/com/example/hackgt/View/ChestActivity.java
+++ app/src/main/java/com/example/hackgt/View/ChestActivity.java
@@ -30,6 +30,7 @@ public class ChestActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ChestActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(intent);
             }
         });
