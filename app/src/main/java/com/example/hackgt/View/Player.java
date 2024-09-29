b73bd5d5d53d5699d0a6e2c23a7a93d93a49a2b5
koan <koan329@gmail.com>
2024-09-28 20:57:26 -0400

    ui changes

--- app/src/main/java/com/example/hackgt/View/Player.java
+++ app/src/main/java/com/example/hackgt/View/Player.java
@@ -6,9 +6,7 @@ import java.util.LinkedList;
 public class Player {
     public static int coins = 0;
     public static int baseSteps = 0;
-    public static int spent = 0;
     public static int[] items = {0, 0 ,0};
     public static int animalProgress = 0;
     public static int animalLevel = 0;
-    public static LinkedList<Chests> chestInventory = new LinkedList<>();
 }
